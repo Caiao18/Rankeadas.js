cce207a85a3c5ef7c37c595f1a6690dd6d83f1bf
Caio Mendes <caiomendes2212@gmail.com>
2024-10-04 21:22:04 -0300

    codigo testado e funcionando

--- Rankeadas.js
+++ Rankeadas.js
@@ -1,4 +1,33 @@
 //Teste Rankeadas
 function calcularNivel(vitorias, derrotas) {
-    
+
+    let saldoDeVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (saldoDeVitorias < 0) {
+        nivel = "Ferro"; 
+    } else if (saldoDeVitorias >= 0 && saldoDeVitorias <= 10) {
+        nivel = "Ferro";
+    } else if (saldoDeVitorias >= 11 && saldoDeVitorias <= 20) {
+    nivel = "Bronze";
+    } else if (saldoDeVitorias >= 21 && saldoDeVitorias <= 50) {
+    nivel = "Prata";
+    } else if (saldoDeVitorias >= 51 && saldoDeVitorias <= 80) {
+    nivel = "Ouro";
+    } else if (saldoDeVitorias >= 81 && saldoDeVitorias <= 90) {
+    nivel = "Diamante";
+    } else if (saldoDeVitorias >= 91 && saldoDeVitorias <= 100) {
+    nivel = "Lendario";
+    } else if (saldoDeVitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+        return { saldoDeVitorias, nivel };        
 }
+
+let vitorias = 68; 
+let derrotas = 17;
+let resultado = calcularNivel(vitorias, derrotas);
+
+console.log("A quantidade de partidas ganhas pelo o Heori foi de " + vitorias);
+console.log("E seu nivel é " + resultado.nivel);
